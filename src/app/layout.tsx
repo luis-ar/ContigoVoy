c61ed43ab9e7c8ddf878e139ae51f46cc2adf6b5
Luis Santos <luisdemaryori@gmail.com>
2025-01-10 10:34:53 -0500

    new mobil

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navbar />
-          <div className="min-h-screen mt-[10vh] absolute ">{children}</div>
+          <div className="min-h-screen mt-[10vh]">{children}</div>
         </ThemeProvider>
       </body>
     </html>
