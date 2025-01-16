8817b2c9b3ca24f27d0266485754596bc46d207f
marycieloSanchez1 <72656839@certus.edu.pe>
2025-01-16 12:44:31 -0500

    tipografia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,15 @@ import { ThemeProvider } from "../components/theme-provider";
 import Navbar from "../components/Navbar";
 import Footer from "./footer";
 import { AuthProvider } from "@/context/authContext";
+import { lexend } from "@/components/ui/fonts";
 
-import { Lexend } from "next/font/google";
-
-export const lexend = Lexend({ subsets: ['latin'] });
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning >
       <body className={`${lexend.className} antialiased`}>
         <ThemeProvider
           attribute="class"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import { usePathname } from "next/navigation";
 
 const navItems = [
   {
-    name: "Home",
+    name: "Sobre Nosotros",
     link: "/",
   },
   {
