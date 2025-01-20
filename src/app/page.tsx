441759635c985f4dbef98b4b923513126c2360bc
marycieloSanchez1 <72656839@certus.edu.pe>
2025-01-20 11:39:22 -0500

    as

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import ChooseUs from "@/components/main/chooseUs";
 import TherapyServices from "@/components/main/therapyServices";
 import OnlinePsychology from "@/components/main/onlinePsychology";
 import About from "@/components/About";
+import Probar from "@/components/NewSlider";
 
 export default function Home() {
   
   return (
     <div className="min-h-screen">
-       <About/>
+       <Probar/>
       <TherapyServices />
       <ChooseUs />
       <OnlinePsychology />
