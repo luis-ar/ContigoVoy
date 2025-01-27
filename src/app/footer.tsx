863120bf75cf2e1134088daf259b296a6c8760a5
marycieloSanchez1 <72656839@certus.edu.pe>
2025-01-27 12:34:19 -0500

    ultimas actualizaciones

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -43,7 +43,7 @@ export default function Footer() {
     <footer className="bg-[#634AE2] text-white">
       <div className="max-w-7xl mx-auto px-4 py-16">
         {/* Main Footer Content */}
-        <div className="flex flex-col md:flex-row justify-between items-start space-y-12 md:space-y-0">
+        <div className="flex flex-col md:flex-row justify-between items-center md:items-start space-y-12 md:space-y-0">
           <motion.div
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -51,7 +51,7 @@ export default function Footer() {
             className="flex-shrink-0"
           >
             <Image
-              src="/LogoBlanco.png"
+              src="/LogoBlanco.webp"
               alt="Contigo Voy Logo"
               width={150}
               height={60}
@@ -59,57 +59,58 @@ export default function Footer() {
           </motion.div>
 
           {/* Footer Sections */}
-          <div className="w-full md:w-auto ml-auto">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-              {footerSections.map((section, idx) => (
-                <motion.div
-                  key={section.title}
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ delay: 0.2 * idx }}
-                  className="relative"
-                >
-                  <h3 className="text-xl font-semibold mb-6">
-                    {section.title}
-                  </h3>
-                  <ul className="space-y-3">
-                    {section.links?.map((link) => (
-                      <li key={link.text}>
-                        <Link
-                          href={link.href}
-                          className="text-purple-100 hover:text-white transition-colors duration-500 text-sm flex items-center group relative"
-                        >
-                          <span className="w-1.5 h-1.5 bg-purple-300 rounded-full mr-2" />
-                          <span className="relative group">
-                            {link.text}
-                            <span className="absolute bottom-0 left-0 w-0 transition-all h-0.5 bg-white group-hover:w-full inline-block duration-500"></span>
-                          </span>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </motion.div>
-              ))}
+          <div className="w-full md:w-auto ml-auto flex flex-col text-center md:text-left items-center justify-start md:items-start">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                {footerSections.map((section, idx) => (
+                  <motion.div
+                    key={section.title}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0.2 * idx }}
+                    className="relative"
+                  >
+                    <h3 className="text-xl font-semibold mb-6">
+                      {section.title}
+                    </h3>
+                    <ul className="space-y-3">
+                      {section.links?.map((link) => (
+                        <li key={link.text}>
+                          <Link
+                            href={link.href}
+                            className="text-purple-100 hover:text-white transition-colors duration-500 text-sm flex items-center group relative"
+                          >
+                            <span className="w-1.5 h-1.5 bg-purple-300 rounded-full mr-2" />
+                            <span className="relative group">
+                              {link.text}
+                              <span className="absolute bottom-0 left-0 w-0 transition-all h-0.5 bg-white group-hover:w-full inline-block duration-500"></span>
+                            </span>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </motion.div>
+                ))}
+              </div>
             </div>
-          </div>
-
-          {/* Iconos de redes sociales */}
-
-          <div className="flex-col items-start justify-start w-full md:w-auto">
-            <h3 className="text-xl font-semibold text-white mb-4">Síguenos</h3>
-            <motion.div
-              variants={containerVariants}
-              initial="hidden"
-              animate="visible"
-              className="flex justify-start space-x-6"
-            >
-               <motion.a>
-                <RedesSociales />
-              </motion.a>
-            </motion.div>
+{/* Iconos de redes sociales */}
+<div className="flex flex-col items-center text-center md:text-left justify-start w-full md:w-auto md:items-start">
+              <h3 className="text-xl font-semibold text-white mb-4">
+                Síguenos
+              </h3>
+              <motion.div
+                variants={containerVariants}
+                initial="hidden"
+                animate="visible"
+                className="flex justify-start space-x-6"
+              >
+                <motion.a>
+                  <RedesSociales />
+                </motion.a>
+              </motion.div>
+            </div>
+          
           </div>
         </div>
-      </div>
     </footer>
   );
 }
