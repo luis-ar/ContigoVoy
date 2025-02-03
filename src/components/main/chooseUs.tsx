375e90fbd2795b4bc3b6052560c4d49b46562467
Toowaa <brahanax@gmail.com>
2025-02-03 13:46:19 -0500

    Fix: Complete all improves to main

--- src/components/main/chooseUs.tsx
+++ src/components/main/chooseUs.tsx
@@ -1,36 +1,112 @@
-'use client'
+"use client";
 
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function ChooseUs() {
   const features = [
     {
-      icon: <Image src={'/ChooseUsImages/especialistas.webp'} alt="especialista" width={70} height={60} />,
+      icon: (
+        <Image
+          src={"/ChooseUsImages/especialistas.webp"}
+          alt="especialista"
+          width={70}
+          height={60}
+        />
+      ),
       title: "Especialistas colegiados",
     },
     {
-      icon: <Image src='/ChooseUsImages/atencionvirtual.webp' alt="Atención virtual" width={70} height={60} />,
+      icon: (
+        <Image
+          src="/ChooseUsImages/atencionvirtual.webp"
+          alt="Atención virtual"
+          width={70}
+          height={60}
+        />
+      ),
       title: "Atención virtual",
     },
     {
-      icon: <Image src='/ChooseUsImages/etica.webp' alt="Ética y confiabilidad" width={70} height={60} />,
+      icon: (
+        <Image
+          src="/ChooseUsImages/etica.webp"
+          alt="Ética y confiabilidad"
+          width={70}
+          height={60}
+        />
+      ),
       title: "Ética y confiabilidad",
     },
     {
-      icon: <Image src='/ChooseUsImages/horarios.webp' alt="Horarios flexibles" width={70} height={60} />,
+      icon: (
+        <Image
+          src="/ChooseUsImages/horarios.webp"
+          alt="Horarios flexibles"
+          width={70}
+          height={60}
+        />
+      ),
       title: "Horarios flexibles",
     },
     {
-      icon: <Image src='/ChooseUsImages/costos.webp' alt="Costos accesibles" width={70} height={60} />,
+      icon: (
+        <Image
+          src="/ChooseUsImages/costos.webp"
+          alt="Costos accesibles"
+          width={70}
+          height={60}
+        />
+      ),
       title: "Costos accesibles",
     },
     {
-      icon: <Image src='/ChooseUsImages/confidencialidad.webp' alt="Confidencialidad" width={70} height={60} />,
+      icon: (
+        <Image
+          src="/ChooseUsImages/confidencialidad.webp"
+          alt="Confidencialidad"
+          width={70}
+          height={60}
+        />
+      ),
       title: "Confidencialidad",
     },
   ];
 
+  const features2 = [
+    {
+      icon:"/ChooseUsImages/especialistas.webp"
+      ,
+      title: "Especialistas colegiados",
+    },
+    {
+      icon:"/ChooseUsImages/atencionvirtual.webp"
+        ,
+      title: "Atención virtual",
+    },
+    {
+      icon:"/ChooseUsImages/etica.webp",
+        
+      title: "Ética y confiabilidad",
+    },
+    {
+      icon: "/ChooseUsImages/horarios.webp"
+        ,
+      title: "Horarios flexibles",
+    },
+    {
+      icon:
+         "/ChooseUsImages/costos.webp"
+       ,
+      title: "Costos accesibles",
+    },
+    {
+      icon:"/ChooseUsImages/confidencialidad.webp",
+         
+      title: "Confidencialidad",
+    },
+ 
+    ]
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -60,9 +136,13 @@ export default function ChooseUs() {
         transition={{ duration: 0.6 }}
         className="text-center mb-12"
       >
-        <h2 className="text-4xl font-bold text-[#634AE2] mb-2">¿Por qué elegirnos?</h2>
+        <h2 className="text-4xl font-bold text-[#634AE2] mb-2">
+          ¿Por qué elegirnos?
+        </h2>
         <p className=" text-[#634AE2] mb-10 w-[643px] mx-auto leading-relaxed ">
-        En Contigo Voy, te ofrecemos atención psicológica online que se adapta a ti, brindándote el apoyo necesario para afrontar los desafíos diarios con mayor fortaleza y equilibrio emocional.
+          En Contigo Voy, te ofrecemos atención psicológica online que se adapta
+          a ti, brindándote el apoyo necesario para afrontar los desafíos
+          diarios con mayor fortaleza y equilibrio emocional.
         </p>
       </motion.div>
 
@@ -80,12 +160,12 @@ export default function ChooseUs() {
             whileHover={{ scale: 1.05 }}
             className="group"
           >
-            <div className="flex flex-col items-center justify-center w-40 h-40 rounded-full bg-[#634AE2]   backdrop-blur-sm transition-all duration-300 cursor-pointer shadow-lg space-y-4">
-              <div className="p-4 bg-[#634AE2] rounded-full group-hover:bg-[#9494F3] transition-colors duration-300">
-                {feature.icon}
+           <div className="flex flex-col items-center justify-center w-40 h-40 rounded-full bg-[#634AE2] backdrop-blur-sm transition-all duration-300 cursor-pointer ">
+                <div className="p-4 bg-[#634AE2] rounded-full group-hover:bg-[#9494F3] transition-colors duration-300">
+                  {feature.icon}
+                </div>
               </div>
-            </div>
-            <h3 className="text-center text-lg font-semibold text-[#634AE2] max-w-[9rem] mt-3">
+              <h3 className="text-center text-lg w-40 font-semibold text-[#634AE2]  mt-3 ">
                 {feature.title}
               </h3>
           </motion.div>
@@ -104,7 +184,7 @@ export default function ChooseUs() {
                   {feature.icon}
                 </div>
               </div>
-              <h3 className="text-center text-lg font-semibold text-[#634AE2] max-w-[8rem] mt-3 ">
+              <h3 className="text-center text-lg w-full font-semibold text-[#634AE2]  mt-3 ">
                 {feature.title}
               </h3>
             </motion.div>
