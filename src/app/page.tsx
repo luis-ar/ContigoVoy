a21664d36e68788ffa17679047a291e1563ca7f4
Toowaa <brahanax@gmail.com>
2025-02-03 11:44:21 -0500

    New: Make new main slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import ChooseUs from "@/components/main/chooseUs";
 import TherapyServices from "@/components/main/therapyServices";
 import OnlinePsychology from "@/components/main/onlinePsychology";
 import Probar from "@/components/NewSlider";
+import MainSlider from "@/components/mainslider";
 
 export default function Home() {
   
   return (
     <div className="min-h-screen">
-      <Probar/>
+    <MainSlider/>
       <TherapyServices />
       <ChooseUs />
       <OnlinePsychology />
