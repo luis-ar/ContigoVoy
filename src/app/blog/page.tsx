59a2077f239270565f115866fee90588307c3e20
Toowaa <brahanax@gmail.com>
2025-02-08 09:39:22 -0500

    feat: Creacion de blog

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import BlogPageComponent from "@/components/BlogPageComponent"
+
+
+
+export default function BlogPage() {
+  return (
+    <>
+    <BlogPageComponent />
+    </>
+  );
+}
