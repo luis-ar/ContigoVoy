0b19ba3be4f4ebdbd2cd9f940dee2118d2e38398
marycieloSanchez1 <72656839@certus.edu.pe>
2025-02-08 09:40:58 -0500

    s

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- /* basePath: "/subdominio", // Ajusta al nombre del subdirectorio en tu subdominio
-  assetPrefix: "https://subdominio.contigo-voy.com", // URL completa del subdominio
+ /*basePath: "/nuevosub", // Ajusta al nombre del subdirectorio en tu subdominio
+  assetPrefix: "https://nuevosub.contigo-voy.com", // URL completa del subdominio
   trailingSlash: true, // Opcional: agrega barras al final de las rutas*/
 };
 
